9f835ded2cb00bfd515e68af622e365e5b71634b
fijibottle <gulfurs@gmail.com>
2023-09-12 19:26:37 +0200

    better bajablast

--- Baja_blast_breakout/Assets/Scripts/bounce.cs
+++ Baja_blast_breakout/Assets/Scripts/bounce.cs
@@ -6,7 +6,8 @@ public class bounce : MonoBehaviour
 {
   public float bounceForce = 10.0f;
 
-    private Rigidbody rb;
+  private Rigidbody rb;
+  private int lives = 3;
 
     void Start()
     {
@@ -17,8 +18,19 @@ public class bounce : MonoBehaviour
     {
          if (collision.gameObject.CompareTag("Platform"))
          {
-            Vector3 bounceDirection = new Vector3(Random.Range(-1f, 1f), 1f, Random.Range(-1f, 1f)).normalized;
+            Vector3 bounceDirection = new Vector3(Random.Range(-2f, 2f), 5f, Random.Range(-2f, 2f)).normalized;
              rb.AddForce(bounceDirection * bounceForce, ForceMode.Impulse);
          }
+         if (collision.gameObject.CompareTag("Ground")){
+            lives--;
+            Vector3 Respawnpos = new Vector3 (0f, 15f, 0f);
+            transform.position = Respawnpos;
+            Debug.Log(lives);
+         }
     }
+  void Update() {
+    if(lives < 1){
+      Debug.Log("TABER!");
+    }
+  }
 }
