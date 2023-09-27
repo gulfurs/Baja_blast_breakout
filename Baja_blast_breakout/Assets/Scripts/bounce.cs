0540e86399d5365aab32abccc2caf88a712f8c18
Simon <gulfurs@gmail.com>
2023-09-27 10:00:09 +0200

    push video

--- Baja_blast_breakout/Assets/Scripts/bounce.cs
+++ Baja_blast_breakout/Assets/Scripts/bounce.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class bounce : MonoBehaviour
 {
-  public float bounceForce = 10.0f;
+  public float bounceForce = 30.0f;
   private Rigidbody rb;
   private int lives = 3;
 
@@ -23,17 +23,14 @@ public class bounce : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-         if (collision.gameObject.CompareTag("Platform"))
-         {
-            Vector3 bounceDirection = new Vector3(Random.Range(-2f, 2f), 5f, Random.Range(-2f, 2f)).normalized;
+         if (collision.gameObject.CompareTag("Platform")){
+            Vector3 bounceDirection = new Vector3(Random.Range(5f, -5f), 5f, Random.Range(-5f, 5f)).normalized;
             rb.AddForce(bounceDirection * bounceForce, ForceMode.VelocityChange);
             score ++;
             scoreText.text = "Score : " + score;
-            
          }
          if (collision.gameObject.CompareTag("Ground")){
             lives--;
-            
             Vector3 Respawnpos = new Vector3 (0f, 15f, 0f);
             transform.position = Respawnpos;
             livesText.text = "Lives : " + lives;
