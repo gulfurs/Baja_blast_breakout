da52c7b379800c5fc1f90c6db63c2ac14f32d628
fijibottle <gulfurs@gmail.com>
2023-10-13 14:02:59 +0200

    gave over GAME OVER

--- Baja_blast_breakout/Assets/Scripts/bounce.cs
+++ Baja_blast_breakout/Assets/Scripts/bounce.cs
@@ -14,31 +14,42 @@ public class bounce : MonoBehaviour
   public TextMeshProUGUI livesText;
   public int score = 0;
 
+  public Animator animator;
+  private bool isDead = false;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         scoreText.text = "Score : " + score;
         livesText.text = "Lives : " + lives;
+
     }
 
     private void OnCollisionEnter(Collision collision)
-    {
+    { 
+      if(!isDead){
          if (collision.gameObject.CompareTag("Platform")){
             Vector3 bounceDirection = new Vector3(Random.Range(5f, -5f), 5f, Random.Range(-5f, 5f)).normalized;
             rb.AddForce(bounceDirection * bounceForce, ForceMode.VelocityChange);
             score ++;
             scoreText.text = "Score : " + score;
          }
-         if (collision.gameObject.CompareTag("Ground")){
-            lives--;
-            Vector3 Respawnpos = new Vector3 (0f, 15f, 0f);
-            transform.position = Respawnpos;
-            livesText.text = "Lives : " + lives;
+        
+          if (collision.gameObject.CompareTag("Ground")){
+              lives--;
+              Vector3 Respawnpos = new Vector3 (0f, 15f, 0f);
+              transform.position = Respawnpos;
+              livesText.text = "Lives : " + lives;
+          }
          }
     }
   void Update() {
-    if(lives < 1){
-      SceneManager.LoadScene(0);
+    if(lives < 1 && !isDead){
+      animator.SetBool("DeathTrigger", true);
+      isDead = true;
+      
+      //SceneManager.LoadScene(0);
+
     }
     PlayerPrefs.SetInt("Score", score);
     PlayerPrefs.Save();
